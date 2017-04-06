5e60b0a86eac4f75feefdca19efc57a3f5ba7c15
diego3 <www.diegosantos.com.br@gmail.com>
2017-04-06 12:46:39 -0300

    - testing on phone test rest

--- app/api/tests/Rest/PhoneTest.php
+++ app/api/tests/Rest/PhoneTest.php
@@ -4,17 +4,22 @@ namespace PhoneApp\Tests\Rest;
 
 use PHPUnit\Framework\TestCase;
 use PhoneApp\Rest\Phone;
+use Httpful\Request;
 
 /**
  * @covers Factory
  */
 class PhoneTest extends TestCase
 {
+     //http://phphttpclient.com/
+     protected $host = "http://localhost/api/";
 
 	   public function testSerchEndPoint(){
           //$rest = new Phone();
           //TODO use a http client to test json responses and http status
           //$rest->search("q=diego");
+          $resource = "phone/search?q=diego";
+          $response = Request::get($this->host.$resource)->expectsJson()->send();
 
           $this->assertEquals(true, true);
      }
