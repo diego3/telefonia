78b5ed278c4d18ccb9f3590209c0bb30468f64f1
Diego Rosa dos Santos <www.diegosantos.com.br@gmail.com>
2017-04-06 09:58:51 -0300

    - added Factory tests - first tests

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+First of all, To you be able to run the phpunit tests the data base need to be created!
+
+Without that the tests will fail!
 
 How to run the phpunit tests
 ===========
---
+++ app/api/tests/FactoryTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace PhoneApp\Tests;
+
+use PHPUnit\Framework\TestCase;
+use PhoneApp\Factory;
+use PhoneApp\Dao\User;
+use PhoneApp\Dao\Phone;
+
+/**
+ * @covers Factory
+ */
+class FactoryTest extends TestCase
+{
+    
+    public function testCreateDbConnectionMethod(){
+        $pdo = Factory::createDbConnection();
+        $this->assertInstanceOf(\PDO::class, $pdo);
+    }
+
+    /**
+     * 
+     * @depends testCreateDbConnectionMethod
+     */
+    public function testCreatePhoneDaoMethod(){
+        $dao = Factory::createPhoneDao();
+        $this->assertInstanceOf(Phone::class, $dao);
+    }
+
+    /**
+     * 
+     * @depends testCreateDbConnectionMethod
+     */
+    public function testCreateUserDaoMethod(){
+        $dao = Factory::createUserDao();
+        $this->assertInstanceOf(User::class, $dao);
+    }
+
+}
