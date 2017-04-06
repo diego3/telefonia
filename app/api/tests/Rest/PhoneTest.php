6c34bc9c6e1a9c8bca09154dfd60743080c2359a
Diego Rosa dos Santos <www.diegosantos.com.br@gmail.com>
2017-04-06 10:15:58 -0300

    - added phone rest test and activated travisci

---
+++ app/api/tests/Rest/PhoneTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace PhoneApp\Tests\Rest;
+
+use PHPUnit\Framework\TestCase;
+use PhoneApp\Rest\Phone;
+
+/**
+ * @covers Factory
+ */
+class PhoneTest extends TestCase
+{
+    
+	
+
+}
