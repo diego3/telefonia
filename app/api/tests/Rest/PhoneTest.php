134ffa056317187ebbbd41313497d53b409af57e
diego3 <www.diegosantos.com.br@gmail.com>
2017-04-06 12:30:08 -0300

    - adjust php version on travis

--- app/api/tests/Rest/PhoneTest.php
+++ app/api/tests/Rest/PhoneTest.php
@@ -10,7 +10,13 @@ use PhoneApp\Rest\Phone;
  */
 class PhoneTest extends TestCase
 {
-    
-	
+
+	   public function testSerchEndPoint(){
+          //$rest = new Phone();
+          //TODO use a http client to test json responses and http status
+          //$rest->search("q=diego");
+
+          $this->assertEquals(true, true);
+     }
 
 }
