1ebbae07b318c02df9ab21ec1ec221f25a45cd7a
diego3 <www.diegosantos.com.br@gmail.com>
2017-04-09 22:52:49 -0300

    - added more one test for Phone DAO class

--- app/api/tests/Dao/PhoneTest.php
+++ app/api/tests/Dao/PhoneTest.php
@@ -19,8 +19,29 @@ class PhoneTest extends TestCase
           $pdo = Factory::createDbConnection();
           $dao = new Phone($pdo);
           $phone = $dao->byId($phoneId);
-          
+
           $this->assertTrue(!empty($phone));
           $this->assertEquals($phoneId, $phone->getId());
+      }
+
+      /**
+      *  @covers \PhoneApp\Dao\Phone::byUserIdAsObject
+      */
+      public function testByUserIdAsObjectWithAValidUserId(){
+          $userid = 3;//admin user
+          $pdo = Factory::createDbConnection();
+          $dao = new Phone($pdo);
+          $phones = $dao->byUserIdAsObject($userid);
+          $this->assertTrue(count($phones) > 3, "Admin user should have at least 4 phones");
+
+          $i = 0;
+          foreach($phones as $phone){
+              if($i == 2){
+                break;
+              }
+              $this->assertTrue(!empty($phone->getId()));
+              $this->assertTrue(!empty($phone->getNumber()));
+              $i++;
+          }
       }
 }
